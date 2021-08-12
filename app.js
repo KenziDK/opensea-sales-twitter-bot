e3f7691fe9c34a607f017b106e2f229844eac914
KenziDK <info@kenzidk.be>
2021-08-12 23:43:55 +0100

    Update app.js

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ async function getLastestSaleData(collection) {
         }
     }
     let res = await axios.post('https://api.opensea.io/graphql/', postData, {
-        headers: {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}
+        headers: {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0'}
     });
     return res.data
 }
